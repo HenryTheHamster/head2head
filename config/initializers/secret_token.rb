d55d9e53933642bee5ca49ab8b51a024f213e68f
Andy Shaw <ashaw@thoughtworks.com>
2013-06-25 15:33:53 +0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Head2head::Application.config.secret_token = '925b4905ffb837023766e2f85184867588285e01a90738e7f1a59ba4964652f20fe1843b2663c38a4b669302a792d5f4efd72c3f774f0adc73995b79b38bcd67'

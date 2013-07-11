b01500b38ad0284ad7653ce8d25dca89de1f4ace
Andy Shaw <ashaw@thoughtworks.com>
2013-07-11 22:15:00 +1000

    DDD

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Head2head::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -45,7 +45,7 @@ Head2head::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
-  config.assets.initialize_on_precompile = false
+  #config.assets.initialize_on_precompile = false
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( match.js )

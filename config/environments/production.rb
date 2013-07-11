51b11abc7133c51584afe3ce57b4e9764d4ae03b
Andy Shaw <ashaw@thoughtworks.com>
2013-07-11 21:18:41 +1000

    More heroku stuff.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,12 @@ Head2head::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
+  config.assets.com
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
@@ -46,7 +48,7 @@ Head2head::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( match.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false

624267a9989c5fa68627cc1c2c298e2cadb697a6
Andy Shaw <ashaw@thoughtworks.com>
2013-07-13 14:11:03 +1000

    Added specs for selecting a team.

--- spec/javascripts/matches/match_view_model_spec.js
+++ spec/javascripts/matches/match_view_model_spec.js
@@ -17,6 +17,20 @@ describe('MatchViewModel', function() {
     expect(viewModel.away_team).toBe('Ninjas');
     expect(viewModel.venue).toBe('The End of the Universe');
     expect(viewModel.home_win).toBe(true);
-  }); 
+  });
+
+  describe('selecting teams', function() {
+    it('should select the home team', function() {
+      viewModel.selectHome();
+      expect(viewModel.home_selected()).toBe(true);
+      expect(viewModel.away_selected()).toBe(false);
+    });
+
+    it('should select the away team', function() {
+      viewModel.selectAway();
+      expect(viewModel.home_selected()).toBe(false);
+      expect(viewModel.away_selected()).toBe(true);
+    });
+  });
 
 });

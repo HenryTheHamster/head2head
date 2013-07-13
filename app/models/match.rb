a8a562ebcd6b95b2f3f8568426e5941fe908cc77
Andy Shaw <ashaw@thoughtworks.com>
2013-07-13 15:47:47 +1000

    Add round to Match.

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,10 @@
 class Match < ActiveRecord::Base
-  attr_accessible :away_team, :home_team, :home_win, :venue, :played_at
+  attr_accessible :away_team, :home_team, :home_win, :venue, :played_at, :round
+  
   has_one :tip
+
+  validates_presence_of :home_team
+  validates_presence_of :away_team
+  validates_presence_of :venue
+  validates_presence_of :played_at
 end
---
+++ db/migrate/20130713054344_add_round_to_match.rb
@@ -0,0 +1,9 @@
+class AddRoundToMatch < ActiveRecord::Migration
+def self.up
+    add_column :matches, :round, :integer
+  end
+
+  def self.down
+    remove_column :matches, :round
+  end
+end
